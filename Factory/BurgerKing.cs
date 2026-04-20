dd09eb70e0272715e15f0db90b66d25c7633eb5b
Daniel <dmribeiro@unifesp.br>
2026-04-20 16:49:47 -0300

    Add menus to mc and bk

--- Factory/BurgerKing.cs
+++ Factory/BurgerKing.cs
@@ -1,9 +1,27 @@
 class BurgerKing : IRestaurant
 {
     private Menu menu;
+
+    public BurgerKing()
+    {
+        menu = new Menu();
+    }
     
     public void SetMenu(Menu menu)
     {
-        // Set Burger King's specific menu
+        // Add Base Items
+        menu.AddItem("CheeseBurger", new CheeseBurger());
+        menu.AddItem("Fries", new Fries());
+        menu.AddItem("Coke", new Coke());
+        menu.AddItem("Guarana Antarctica", new GuaranaAntarctica());
+        menu.AddItem("Milkshake", new Milkshake());
+
+        // Add specifics
+        menu.AddItem("Whopper", new ExtraCheese(new Salad(new CheeseBurger())));
+        menu.AddItem("Whopper Bacon", new Bacon(new ExtraCheese(new Salad(new CheeseBurger()))));
+        menu.AddItem("Double Whopper", new ExtraPatty(new ExtraCheese(new Salad(new CheeseBurger()))));
+        menu.AddItem("Strawberry Milkshake", new Strawberry(new Milkshake()));
+
+        /* Note: Add Salad and Strawberry decorators */
     }
 }
--- Factory/McDonalds.cs
+++ Factory/McDonalds.cs
@@ -2,8 +2,25 @@ class McDonalds : IRestaurant
 {
     private Menu menu;
 
+    public McDonalds()
+    {
+        menu = new Menu();
+    }
+
     public void SetMenu(Menu menu)
     {
-        // Set McDonald's specific menu
+        // Add Base Items
+        menu.AddItem("CheeseBurger", new CheeseBurger());
+        menu.AddItem("Fries", new Fries());
+        menu.AddItem("Coke", new Coke());
+        menu.AddItem("Guarana Antarctica", new GuaranaAntarctica());
+        menu.AddItem("Milkshake", new Milkshake());
+
+        // Add specifics
+        menu.AddItem("Big Mac", new ExtraBun(new ExtraCheese(new ExtraPatty(new CheeseBurger()))));
+        menu.AddItem("Big Mac Bacon", new Bacon(new ExtraCheese(new ExtraPatty(new CheeseBurger()))));
+        menu.AddItem("Chocolate Milkshake", new Chocolate(new Milkshake()));
+
+        /* Note: Add Bun decorator */
     }
 }
